c8140fdfea104cc7b9d078a21b96e57eecbdbbb8
EnmanuelUrena <71359268+EnmanuelUrena@users.noreply.github.com>
2020-09-15 21:07:54 -0400

    Add files via upload

---
+++ Registro de Datos v1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Registro_de_Datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter Archivo = new StreamWriter (args[0],true);
+            string nombre, apellido, cedula;
+            int edad;
+            Console.Write("Ingrese el nombre: ");
+            nombre = Console.ReadLine(); 
+            Console.Write("Ingrese el apellido: ");
+            apellido = Console.ReadLine(); 
+            Console.Write("Ingrese la cedula: ");
+            cedula = Console.ReadLine();
+            Console.Write("Ingrese la edad: ");
+            edad = Int32.Parse(Console.ReadLine()); 
+            Archivo.WriteLine("{0},{1},{2},{3}",nombre,apellido,cedula,edad);
+            Archivo.Close();
+            Console.WriteLine("Datos agregados {0},{1},{2},{3}",nombre,apellido,cedula,edad);
+
+        }
+     
+    }
+}
